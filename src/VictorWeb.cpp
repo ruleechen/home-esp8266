3a512e33cb90723db47535233a60bfefbc1a8bbc
Rulee Chen <rulee@live.cn>
2022-01-04 14:03:34 +0800

    chore(web): update file resource ttl to 30 minutes

--- src/VictorWeb.cpp
+++ src/VictorWeb.cpp
@@ -33,8 +33,8 @@ namespace Victor::Components {
   }
 
   void VictorWeb::_registerHandlers() {
-    _server->serveStatic("/style.min.css", LittleFS, "/web/style.min.css", "max-age=43200"); // 43200 seconds --> 12 hours
-    _server->serveStatic("/mithril.min.js", LittleFS, "/web/mithril.min.js", "max-age=43200");
+    _server->serveStatic("/style.min.css", LittleFS, "/web/style.min.css", "max-age=1800"); // 1800 seconds --> 0.5 hours
+    _server->serveStatic("/mithril.min.js", LittleFS, "/web/mithril.min.js", "max-age=1800");
     _server->serveStatic("/app.min.js", LittleFS, "/web/app.min.js");
     _server->on(F("/"), HTTP_GET, std::bind(&VictorWeb::_handleIndexPage, this));
     _server->on(F("/system/status"), HTTP_GET, std::bind(&VictorWeb::_handleSystemStatus, this));
