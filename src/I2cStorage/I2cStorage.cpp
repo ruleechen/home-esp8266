4219727f03ddbe4248a0257ccbc9a69071e8ea25
rulee.chen <rulee.chen@ringcentral.com>
2022-06-11 23:16:50 +0800

    feat(i2c): add `enable` pin setting

--- src/I2cStorage/I2cStorage.cpp
+++ src/I2cStorage/I2cStorage.cpp
@@ -6,14 +6,22 @@ namespace Victor::Components {
     _maxSize = 128;
   }
 
-  void I2cStorage::_serializeTo(const I2cConfig& model, DynamicJsonDocument& doc) {
+  void I2cStorage::_serializeTo(const I2cSetting& model, DynamicJsonDocument& doc) {
     doc[F("sda")] = model.sdaPin;
     doc[F("scl")] = model.sclPin;
+    // enable pin
+    const JsonArray enableArr = doc.createNestedArray(F("en"));
+    enableArr[0] = model.enablePin;
+    enableArr[1] = model.enableTrueValue;
   }
 
-  void I2cStorage::_deserializeFrom(I2cConfig& model, const DynamicJsonDocument& doc) {
+  void I2cStorage::_deserializeFrom(I2cSetting& model, const DynamicJsonDocument& doc) {
     model.sdaPin = doc[F("sda")];
     model.sclPin = doc[F("scl")];
+    // enable pin
+    const auto enableArr = doc[F("en")];
+    model.enablePin = enableArr[0];
+    model.enableTrueValue = enableArr[1];
   }
 
 } // namespace Victor::Components
--- src/I2cStorage/I2cStorage.h
+++ src/I2cStorage/I2cStorage.h
@@ -6,18 +6,21 @@
 namespace Victor::Components {
 
   // https://zhuanlan.zhihu.com/p/137568249
-  struct I2cConfig {
+  struct I2cSetting {
     uint8_t sdaPin = 4; // (0 ~ 256) Inter-Integrated Circuit - Serial Data (I2C-SDA)
     uint8_t sclPin = 5; // (0 ~ 256) Inter-Integrated Circuit - Serial Clock (I2C-SCL)
+    // enable pin
+    int8_t enablePin = -1; // (-127 ~ 128)
+    uint8_t enableTrueValue = 0; // (0 ~ 256) LOW
   };
 
-  class I2cStorage : public FileStorage<I2cConfig> {
+  class I2cStorage : public FileStorage<I2cSetting> {
    public:
     I2cStorage(const char* filePath = "/i2c.json");
 
    protected:
-    void _serializeTo(const I2cConfig& model, DynamicJsonDocument& doc) override;
-    void _deserializeFrom(I2cConfig& model, const DynamicJsonDocument& doc) override;
+    void _serializeTo(const I2cSetting& model, DynamicJsonDocument& doc) override;
+    void _deserializeFrom(I2cSetting& model, const DynamicJsonDocument& doc) override;
   };
 
 } // namespace Victor::Components
